b8a14d4d5899feac8178bdd675030d8ff3b4b0fb
Eszter Viczian <eviczia@gmail.com>
2019-10-24 11:04:27 +0200

    diamond excersise with two middle lines

--- week-03/day-03/src/DrawDiamondWithTwoMiddleLines.java
+++ week-03/day-03/src/DrawDiamondWithTwoMiddleLines.java
@@ -5,38 +5,66 @@ public class DrawDiamondWithTwoMiddleLines {
         System.out.println("Please enter a whole number!");
         Scanner scanner = new Scanner(System.in);
         int userInput = scanner.nextInt();
-        if ((userInput % 2) ==0) {
-            userInput += 1;
-        }
-
-        int line = 1;
-        while (line <= ((userInput+1)/2)) {
-            int starNr = 1;
-            int spaceNr = 1;
-            while (spaceNr <= ((userInput+1)/2) - line) {
-                System.out.print(" ");
-                spaceNr ++;
+        if (!((userInput % 2) ==0)) {
+            int line = 1;
+            while (line <= ((userInput + 1) / 2)) {
+                int starNr = 1;
+                int spaceNr = 1;
+                while (spaceNr <= ((userInput + 1) / 2) - line) {
+                    System.out.print(" ");
+                    spaceNr++;
+                }
+                while (starNr <= (2 * line - 1)) {
+                    System.out.print("*");
+                    starNr++;
+                }
+                System.out.println();
+                line++;
             }
-            while (starNr <= (2 * line - 1)) {
-                System.out.print("*");
-                starNr++;
+            while (line <= userInput) {
+                int starNr = 1;
+                int spaceNr = 1;
+                while (spaceNr <= (line - (userInput + 1) / 2)) {
+                    System.out.print(" ");
+                    spaceNr++;
+                }
+                while (starNr <= (2 * (userInput - line) + 1)) {
+                    System.out.print("*");
+                    starNr++;
+                }
+                System.out.println();
+                line++;
             }
-            System.out.println();
-            line ++;
-        }
-        while (line <= userInput) {
-            int starNr = 1;
-            int spaceNr = 1;
-            while (spaceNr <= (line - (userInput+1)/2)) {
-                System.out.print(" ");
-                spaceNr ++;
+        } else {
+            int line = 1;
+            while (line <= ((userInput + 1) / 2)) {
+                int starNr = 1;
+                int spaceNr = 1;
+                while (spaceNr <= ((userInput + 1) / 2) - line) {
+                    System.out.print(" ");
+                    spaceNr++;
+                }
+                while (starNr <= (2 * line - 1)) {
+                    System.out.print("*");
+                    starNr++;
+                }
+                System.out.println();
+                line++;
             }
-            while (starNr <= (2 * (userInput - line)+1)) {
-                System.out.print("*");
-                starNr++;
+            while (line <= userInput) {
+                int starNr = 1;
+                int spaceNr = 1;
+                while (spaceNr < (line - (userInput + 1) / 2)) {
+                    System.out.print(" ");
+                    spaceNr++;
+                }
+                while (starNr <= (2 * (userInput - line) + 1)) {
+                    System.out.print("*");
+                    starNr++;
+                }
+                System.out.println();
+                line++;
             }
-            System.out.println();
-            line ++;
         }
     }
 }
