10088910876a5a5db9603006334bc80f99f914e6
Eszter Viczian <eviczia@gmail.com>
2019-10-24 10:57:04 +0200

    earlier files + diamond drawings

---
+++ week-03/day-03/src/DrawDiamondWithTwoMiddleLines.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class DrawDiamondWithTwoMiddleLines {
+    public static void main(String[] args) {
+        System.out.println("Please enter a whole number!");
+        Scanner scanner = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+        if ((userInput % 2) ==0) {
+            userInput += 1;
+        }
+
+        int line = 1;
+        while (line <= ((userInput+1)/2)) {
+            int starNr = 1;
+            int spaceNr = 1;
+            while (spaceNr <= ((userInput+1)/2) - line) {
+                System.out.print(" ");
+                spaceNr ++;
+            }
+            while (starNr <= (2 * line - 1)) {
+                System.out.print("*");
+                starNr++;
+            }
+            System.out.println();
+            line ++;
+        }
+        while (line <= userInput) {
+            int starNr = 1;
+            int spaceNr = 1;
+            while (spaceNr <= (line - (userInput+1)/2)) {
+                System.out.print(" ");
+                spaceNr ++;
+            }
+            while (starNr <= (2 * (userInput - line)+1)) {
+                System.out.print("*");
+                starNr++;
+            }
+            System.out.println();
+            line ++;
+        }
+    }
+}
---
+++ week-03/day-03/src/DrawSquare.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DrawSquare {
+    // Write a program that reads a number from the standard input, then draws a
+    // square like this:
+    //
+    //
+    // %%%%%%
+    // %    %
+    // %    %
+    // %    %
+    // %    %
+    // %%%%%%
+    //
+    // The square should have as many lines as the number was
+    public static void main(String[] args) {
+        System.out.println("Please enter a whole number!");
+        Scanner scanner = new Scanner(System.in);
+        int userInput = scanner.nextInt();
+    }
+
+}
