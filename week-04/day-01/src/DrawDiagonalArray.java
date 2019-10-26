f5d6f12a6a59482873cc8efc4ade55d8495ff749
Eszter Viczian <eviczia@gmail.com>
2019-10-26 13:32:28 +0200

    DrawDiagonal, using arrays

--- week-04/day-01/src/DrawDiagonalArray.java
+++ week-04/day-01/src/DrawDiagonalArray.java
@@ -14,15 +14,25 @@ public class DrawDiagonalArray {
     // The square should have as many lines as the number was
     public static void main(String[] args) {
         int side = _PleaseEnterWholeNumber.getUserInput();
-        Integer [][] diagonal = new Integer[side][side];
+        int[][] diagonal = new int[side][side];
         for (int i = 0; i < diagonal.length; i++) {
             for (int j = 0; j < diagonal[i].length; j++) {
-                if ((i==1)|(j==1)|(i==side)|(j==side)|(i==j)) {
+                if ((i == 0) | (j == 0) | (i == (side - 1) | (j == (side - 1)) | (i == j))) {
                     diagonal[i][j] = 1;
                 } else {
                     diagonal[i][j] = 0;
                 }
             }
         }
+            for (int i = 0; i < diagonal.length; i++) {
+                for (int j = 0; j < diagonal[i].length; j++) {
+                    if (diagonal[i][j]==0) {
+                        System.out.print(" ");
+                    } else {
+                        System.out.print("%");
+                    }
+                }
+                System.out.println();
+            }
     }
 }
