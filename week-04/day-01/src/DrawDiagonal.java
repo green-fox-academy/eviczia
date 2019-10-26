7f96d59d1128e89e2c4eafeb5d8646378c1d7356
Eszter Viczian <eviczia@gmail.com>
2019-10-26 07:43:59 +0200

    DrawDiagonal - now perfect

--- week-04/day-01/src/DrawDiagonal.java
+++ week-04/day-01/src/DrawDiagonal.java
@@ -1,4 +1,3 @@
-
 public class DrawDiagonal {
     // Write a program that reads a number from the standard input, then draws a
     // square like this:
@@ -13,29 +12,26 @@ public class DrawDiagonal {
     // The square should have as many lines as the number was
     public static void main(String[] args) {
         int userInput = _PleaseEnterWholeNumber.getUserInput();
-
         drawHorizontal(userInput);
         drawVerticals(userInput);
         drawHorizontal(userInput);
-
     }
-
     private static void drawHorizontal(int limit) {
         for (int j = 0; j < limit; j++) {
             System.out.print("%");
         }
         System.out.println();
     }
-
     private static void drawVerticals(int limit) {
         for (int line = 1; line < (limit - 1); line++) {
             System.out.print("%");
             int spaceNr = 1;
-            while ((spaceNr >= 1) & (spaceNr < limit - 2)) {
+            while ((spaceNr >= 1) & (spaceNr < limit - 1)) {
                 if (spaceNr == line) {
                     System.out.print("%");
+                } else {
+                    System.out.print(" ");
                 }
-                System.out.print(" ");
                 spaceNr++;
             }
             System.out.println("%");
