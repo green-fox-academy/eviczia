b03b510913a04b744d2933982ff7d83a82ee09ac
Eszter Viczian <eviczia@gmail.com>
2019-10-27 05:47:41 +0100

    DrawChess finished

---
+++ week-04/day-01/src/DrawChess.java
@@ -0,0 +1,33 @@
+public class DrawChess {
+    // Crate a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
+    public static void main(String[] args) {
+        String[][] chessTable = new String[8][8];
+        for (int i = 0; i < chessTable.length; i++) {
+            for (int j = 0; j < chessTable.length; j++) {
+                if (((i % 2) == 0 & (j % 2) == 0) | (!((i % 2) == 0) & !((j % 2) == 0))) {
+                    chessTable[i][j] = "%";
+                } else {
+                    chessTable[i][j] = " ";
+                }
+            }
+        }
+        for (int i = 0; i < chessTable.length; i++) {
+            for (int j = 0; j < chessTable[i].length; j++) {
+                System.out.print(chessTable[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
