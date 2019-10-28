321ea21f797790c57c67fe9dcaa69ae9afbeed5b
Eszter Viczian <eviczia@gmail.com>
2019-10-28 18:51:33 +0100

    this is to test things

---
+++ week-04/day-01/src/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        String proba = "probalkozasok";
+        System.out.println(proba.substring(10, 13));
+        System.out.println(proba.indexOf("sok"));
+    }
+}
