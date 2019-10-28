57472fd7651c05f6f3b9cc373f8d4666a9df650f
Eszter Viczian <eviczia@gmail.com>
2019-10-28 10:59:51 +0100

    Completed excercise "Matrix"

---
+++ week-05/day-01/src/DiagonalMatrix.java
@@ -0,0 +1,26 @@
+public class DiagonalMatrix {
+    // - Create (dynamically) a two dimensional array
+    //   with the following matrix. Use a loop!
+    //
+    //   1 0 0 0
+    //   0 1 0 0
+    //   0 0 1 0
+    //   0 0 0 1
+    //
+    // - Print this two dimensional array to the output
+    public static void main(String[] args) {
+        int side = 4;
+        byte[][] diagonal = new byte [side][side];
+        for (int i = 0; i < diagonal.length; i++) {
+            for (int j = 0; j < diagonal.length; j++) {
+                if (i==j) {
+                    diagonal[i][j] = 1;
+                } else {
+                    diagonal[i][j] = 0;
+                }
+                System.out.print(diagonal[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
