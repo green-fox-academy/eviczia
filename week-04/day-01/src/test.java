df4abe425c5d129bd2c1621c702896ed02624666
Eszter Viczian <eviczia@gmail.com>
2019-10-29 11:53:09 +0100

    SubInt trying with integer to string, need to replace "not"s

--- week-04/day-01/src/test.java
+++ week-04/day-01/src/test.java
@@ -2,6 +2,6 @@ public class test {
     public static void main(String[] args) {
         String proba = "probalkozasok";
         System.out.println(proba.substring(10, 13));
-        System.out.println(proba.indexOf("sok"));
+        System.out.println(proba.indexOf("gumi"));
     }
 }
--- week-05/day-02/src/SubInt.java
+++ week-05/day-02/src/SubInt.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SubInt {
     public static void main(String[] args) {
         //  Create a function that takes a number and an array of integers as a parameter
@@ -5,19 +7,23 @@ public class SubInt {
         //  Or returns an empty array if the number is not part of any of the integers in the array
 
         //  Example:
-        System.out.println(subInt(1, new int[] {1, 11, 34, 52, 61}));
+        System.out.println(Arrays.toString(subInt(1, new int[] {1, 11, 34, 52, 61})));
         //  should print: `[0, 1, 4]`
-        System.out.println(subInt(9, new int[] {1, 11, 34, 52, 61}));
+        System.out.println(Arrays.toString(subInt(9, new int[] {1, 11, 34, 52, 61})));
         //  should print: '[]'
     }
 
     private static String[] subInt(int i, int[] ints) {
+        String iS = Integer.toString(i);
+        String[] intsS = new String[ints.length];
         for (int j = 0; j < ints.length; j++) {
-            String[] stringArray = new String[ints.length],
-            stringArray[j] = Integer.toString([j]);
-            if
-            return stringArray;
-
+            intsS[j] = Integer.toString(ints[j]);
+            if (intsS[j].indexOf(iS) == -1) {
+                intsS[j] = "not";
+            } else {
+                intsS[j] = Integer.toString(j);
+            }
         }
+        return intsS;
     }
 }
