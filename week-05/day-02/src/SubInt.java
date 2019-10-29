08c2edd54643e80238a8a9635b740617a46fa8ce
Eszter Viczian <eviczia@gmail.com>
2019-10-29 11:33:04 +0100

    SubInt trying with integer to string

--- week-05/day-02/src/SubInt.java
+++ week-05/day-02/src/SubInt.java
@@ -1,26 +1,23 @@
-import java.util.Arrays;
-
 public class SubInt {
     public static void main(String[] args) {
         //  Create a function that takes a number and an array of integers as a parameter
-        int number = 1;
-        int wrongNumber = 9;
-        int[] array = {1, 11, 34, 52, 61};
-
         //  Returns the indices of the integers in the array of which the first number is a part of
-        System.out.println(subInt(number, array));
         //  Or returns an empty array if the number is not part of any of the integers in the array
-//        System.out.println(subInt(wrongNumber, array));
+
         //  Example:
- //       System.out.println(subInt(1, new int[] {1, 11, 34, 52, 61}));
+        System.out.println(subInt(1, new int[] {1, 11, 34, 52, 61}));
         //  should print: `[0, 1, 4]`
- //       System.out.println(subInt(9, new int[] {1, 11, 34, 52, 61}));
+        System.out.println(subInt(9, new int[] {1, 11, 34, 52, 61}));
         //  should print: '[]'
     }
-    private static int[] subInt(int i, int[] ints) {
-        int[] output = new int [2];
-        switch (Arrays.binarySearch(ints, i)) {
+
+    private static String[] subInt(int i, int[] ints) {
+        for (int j = 0; j < ints.length; j++) {
+            String[] stringArray = new String[ints.length],
+            stringArray[j] = Integer.toString([j]);
+            if
+            return stringArray;
+
         }
-        return output[];
     }
 }
---
+++ week-05/day-02/src/Unique.java
@@ -0,0 +1,16 @@
+/*public class Unique {
+    public static void main(String[] args) {
+        //  Create a function that takes a list of numbers as a parameter
+        int[] listNumbers = {2, 3, 4, 3, 2, 2, 6, 5, 8, 4, 23};
+        //  Returns a list of numbers where every number in the list occurs only once
+        int[] unique = unique(listNumbers);
+
+        //  Example
+        System.out.println(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34}));
+        //  should print: `[1, 11, 34, 52, 61]`
+    }
+
+    private static int[] unique(int[] listNumbers) {
+
+    }
+}*/
