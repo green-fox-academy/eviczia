2b9a95f2d2ac2357b7eb5d3e54a4ca480a8eab93
Eszter Viczian <eviczia@gmail.com>
2019-11-03 06:53:23 +0100

    Shopping list finished

---
+++ week-05/day-03/src/Shopping_list.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Shopping_list {
+    //We are going to represent a shopping list in a list containing strings.//
+    public static void main(String[] args) {
+    //Create a list with the following items.
+    //Eggs, milk, fish, apples, bread and chicken
+        ArrayList<String> shoppingList = new ArrayList<>();
+        shoppingList.addAll(Arrays.asList("Eggs", "milk", "fish", "apples", "bread", "chicken"));
+    //Create an application which solves the following problems.
+    //Do we have milk on the list?
+        System.out.println(shoppingList.contains("milk"));
+        doesContain(shoppingList, "milk");
+    //Do we have bananas on the list?
+        System.out.println(shoppingList.contains("bananas"));
+        doesContain(shoppingList, "bananas");
+    }
+
+    public static void doesContain (ArrayList list, String item) {
+        String well = "";
+        if (list.contains(item)) {
+            System.out.println("Yep, it's there");
+        } else {
+            System.out.println("Nope, it's not there");
+        };
+    }
+}
+
