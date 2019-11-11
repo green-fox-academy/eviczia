6fb2ffb16d97bb4cbeafd2ac2dc953580b4244a7
Eszter Viczian <eviczia@gmail.com>
2019-11-11 15:51:39 +0100

    WriteSingleLine finished

---
+++ week-07/day-01-File_manipulatio/src/WriteSingleLine.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+public class WriteSingleLine {
+    public static void main(String[] args) {
+        // Write a function that is able to manipulate a file
+        // By writing your name into it as a single line
+        // The file should be named "my-file.txt"
+        // In case the program is unable to write the file,
+        // It should print the following error message: "Unable to write file: my-file.txt"
+
+        String myFilesName = "my-file.txt";
+        Path lookHere = Paths.get("my-file.txt");
+
+        List<String> content = new ArrayList<>();
+ /*       try {
+            content = Files.readAllLines(lookHere);
+        } catch (IOException e) {
+            System.out.println("Sorry, cannot read 'my-file.txt'");
+        }
+        System.out.println(content);*/
+        try {
+            content.add("Eszter Viczian");
+            Files.write(lookHere, content);
+        } catch (IOException e) {
+            System.out.println("Unable to write file: my-file.txt");
+        }
+        System.out.println(content);
+    }
+}
