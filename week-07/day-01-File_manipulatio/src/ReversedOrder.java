14c6c015aa09b2d39d856aa5eb9cb21371e2fdbc
Eszter Viczian <eviczia@gmail.com>
2019-11-13 10:47:41 +0100

    Reversed Order finished

--- week-07/day-01-File_manipulatio/src/ReversedOrder.java
+++ week-07/day-01-File_manipulatio/src/ReversedOrder.java
@@ -8,13 +8,12 @@ import java.util.List;
 public class ReversedOrder {
 
     public static void main(String[] args) {
-    // Create a method that decrypts reversed-order.txt
-        Path pathToReOrder = Paths.get("reversed-lines.txt");
+        // Create a method that decrypts reversed-order.txt
+        Path pathToReOrder = Paths.get("reversed-order.txt");
         reOrder(pathToReOrder);
-        // Create a method that decrypts reversed-lines.txt
     }
 
-    private static void reOrder (Path pathToReOrder) {
+    private static void reOrder(Path pathToReOrder) {
         List<String> content = new ArrayList<>();
         List<String> newContent = new ArrayList<>();
 
@@ -23,10 +22,14 @@ public class ReversedOrder {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(content);
         for (int i = 0; i < content.size(); i++) {
-            newContent.add(content.get(content.size() - i-1));
+            newContent.add(content.get(content.size() - i - 1));
         }
+
         Path pathOfReOrdered = Paths.get("reversed-order-done.txt");
+
+        System.out.println(newContent);
         try {
             Files.write(pathOfReOrdered, newContent);
         } catch (IOException e) {
@@ -34,3 +37,4 @@ public class ReversedOrder {
         }
     }
 }
+
