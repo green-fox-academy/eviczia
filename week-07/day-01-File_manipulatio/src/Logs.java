54ffe1c2639500d3bdb654ce0d1c15415a25da4f
Eszter Viczian <eviczia@gmail.com>
2019-11-14 11:10:37 +0100

    Iterated through Logs 2d array to filter duplicates, return unique items in array

--- week-07/day-01-File_manipulatio/src/Logs.java
+++ week-07/day-01-File_manipulatio/src/Logs.java
@@ -13,25 +13,46 @@ public class Logs {
 // Write a function that returns the GET / POST request ratio.
     public static void main(String[] args) {
         Path logFileHere = Paths.get("log.txt");
-        uniqueIP(logFileHere);
+        System.out.println(Arrays.toString(uniqueIP(logFileHere)));
+        System.out.println(uniqueIP(logFileHere).length);
     }
 
-    private static void uniqueIP(Path logFileHere) {
+    private static String[] uniqueIP(Path logFileHere) {
         List<String> logContent = new ArrayList<>();
+        // read logfile into an ArrayList
         try {
             logContent = Files.readAllLines(logFileHere);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
+        // put logcontent into a 2D Array
         String[][] arrayLine = new String[logContent.size()][];
         for (int i = 0; i < logContent.size(); i++) {
-           arrayLine[i] = logContent.get(i).split(" ");
-           arrayLine[i].
-           System.out.println(Arrays.toString(arrayLine[i]));
-            }
-            System.out.println();
+            arrayLine[i] = logContent.get(i).split(" ");
+ //           System.out.println((arrayLine[i][8]));
         }
+
+        // iterate through the line of the array, checking for IP duplicates (8th column),
+        // rewrite each line with the next one if duplicate, decrease n with every find
+        int n = arrayLine.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; ) {
+                if (arrayLine[i][8].equals(arrayLine[j][8])) {
+                    for (int k = j; k < n - 1; k++) {
+                        arrayLine[k] = arrayLine[k + 1];
+                    }
+                    n--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        String[] uniqueIP = new String[n];
+        for (int i = 0; i <n; i++) {
+            uniqueIP[i] = arrayLine[i][8];
+        }
+        return uniqueIP;
     }
+}
 
