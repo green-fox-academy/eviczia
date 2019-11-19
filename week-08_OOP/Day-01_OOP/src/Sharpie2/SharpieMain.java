e589bc43b3043ea5490ee41364b433f6c9f51ad6
Eszter Viczian <eviczia@gmail.com>
2019-11-19 12:45:07 +0100

    SharpieSet done added comments

--- week-08_OOP/Day-01_OOP/src/Sharpie2/SharpieMain.java
+++ week-08_OOP/Day-01_OOP/src/Sharpie2/SharpieMain.java
@@ -2,6 +2,7 @@ package Sharpie2;
 
 public class SharpieMain {
     public static void main(String[] args) {
+        // these are my sharpies
         Sharpie yellow = new Sharpie("yellow", 6);
         Sharpie red = new Sharpie("red", 5);
         Sharpie blue = new Sharpie("blue", 4);
@@ -9,6 +10,8 @@ public class SharpieMain {
         Sharpie green = new Sharpie("green", 4);
         Sharpie pink = new Sharpie("pink", 6);
 
+
+        // add my new sharpies to my set
         SharpieSet myUsableSharpies = new SharpieSet();
         myUsableSharpies.sharpies.add(yellow);
         myUsableSharpies.sharpies.add(red);
@@ -17,10 +20,11 @@ public class SharpieMain {
         myUsableSharpies.sharpies.add(green);
         myUsableSharpies.sharpies.add(pink);
 
+        // admire my beautiful set of sharpies and count them
         System.out.println(myUsableSharpies);
         System.out.println(myUsableSharpies.countUsable());
 
-
+        // start using my sharpies
         for (int i = 0; i < 100; i++) {
             yellow.use();
             if ( i % 3 == 0) {
@@ -29,12 +33,16 @@ public class SharpieMain {
                 pink.use();
             }
         }
-        System.out.println(myUsableSharpies);
-        System.out.println(myUsableSharpies.countUsable());
-        myUsableSharpies.removeTrash();
+        // take a look at my sharpie set, and count usables
         System.out.println(myUsableSharpies);
         System.out.println(myUsableSharpies.countUsable());
 
+        //throw out the useless ones
+        myUsableSharpies.removeTrash();
+
+        // admire my beautiful set of sharpies and count them
+        System.out.println(myUsableSharpies);
+        System.out.println(myUsableSharpies.countUsable());
 
     }
 }
