1931f9917c6a40f2651c789068cd7128a532e2b2
Eszter Viczian <eviczia@gmail.com>
2019-11-21 13:40:03 +0100

    GreatestCommonDivisor  finished

---
+++ week-08_OOP/day-04-recursion/src/GreatestCommonDivisor.java
@@ -0,0 +1,21 @@
+public class GreatestCommonDivisor {
+    //Greatest Common Divisor
+    //Find the greatest common divisor of two numbers using recursion.
+    public static void main(String[] args) {
+        System.out.println(greatestCommonDivisor(10, 225));
+    }
+
+    private static int greatestCommonDivisor(int num1, int num2) {
+        int possibleDivisor = Math.min(num1, num2);
+        return findGCD (num1, num2, possibleDivisor);
+    }
+
+    public static int findGCD(int num1, int num2, int possibleDivisor) {
+        if ((num1 % possibleDivisor)==0 & (num2 % possibleDivisor)==0) {
+            return possibleDivisor;
+        } else {
+            return findGCD(num1, num2, possibleDivisor-1);
+        }
+
+    }
+}
