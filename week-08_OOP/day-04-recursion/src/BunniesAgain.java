9498d7945bf7672fee8790f225a9109bbe9d1fa7
Eszter Viczian <eviczia@gmail.com>
2019-11-21 14:03:26 +0100

    started BunniesAgain

---
+++ week-08_OOP/day-04-recursion/src/BunniesAgain.java
@@ -0,0 +1,21 @@
+public class BunniesAgain {
+    //Bunnies again
+    //We have bunnies standing in a line, numbered 1, 2, ...
+    // The odd bunnies (1, 3, ..) have the normal 2 ears.
+    //
+    // The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot.
+    // Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+    public static void main(String[] args) {
+        int bunnies = 30;
+        System.out.printf("%d bunnies have %d floppy ears", bunnies, countBunniesEars(bunnies));
+    }
+
+    private static int countBunniesEars(int bunnies) {
+        if (bunnies == 0) {
+            return 0; //no bunnies have no ears
+        } if else {
+            return countBunniesEars(bunnies-1)+2; // it is not a bunny, just a pair of floppy ears
+        }
+    }
+}
