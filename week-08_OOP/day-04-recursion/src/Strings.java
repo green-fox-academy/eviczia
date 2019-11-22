d29a5e4cba908ed1e40f574a10a27f7186cc692c
Eszter Viczian <eviczia@gmail.com>
2019-11-22 10:55:35 +0100

    Strings finished with help

--- week-08_OOP/day-04-recursion/src/Strings.java
+++ week-08_OOP/day-04-recursion/src/Strings.java
@@ -7,17 +7,18 @@ public class Strings {
 
         String stringWithX = "xax, xou did it!";
         System.out.println(stringWithX.replace('x', 'y'));
-        System.out.println(stringWithX.split("x"));
 
-//        System.out.println(xIsY(stringWithX));
+        System.out.println(xIsY(stringWithX));
     }
-
- /*   private static String xIsY(String x) {
-        x.split("x");
-        if (x.charAt(x.length()-1) == 'x'){
-            return xIsY(x.substring(0, x.length()-2)) + "y";
+// https://github.com/green-fox-academy/ZaitzeV16/blob/master/week-03/day-5/src/Strings.java
+    private static String xIsY(String x) {
+        if (x.length() == 0) {
+            return x;
         } else {
-            return xIsY(x.substring(0, x.length()-1));
+            if ("x".equals(String.valueOf(x.charAt(0)))) {//2. if the first char IS x,
+                x = "y" + x.substring(1);           // 3. overwrite string with y instead of x
+            }
+            return x.charAt(0) + xIsY(x.substring(1)); // 1. if first char is not x, keep first char, send the rest back to the function
         }
-    }*/
+    }
 }
