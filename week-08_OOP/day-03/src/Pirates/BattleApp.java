11281f7a3e44a0cc6c6f05c2e6db91be86c40fb6
Eszter Viczian <eviczia@gmail.com>
2019-11-22 11:16:00 +0100

    Created Pirates exercise

---
+++ week-08_OOP/day-03/src/Pirates/BattleApp.java
@@ -0,0 +1,8 @@
+// BattleApp
+//Create a BattleApp class with a main method
+//Create 2 ships, fill them with pirates
+//Have a battle!
+package Pirates;
+
+public class BattleApp {
+}
---
+++ week-08_OOP/day-03/src/Pirates/WarApp.java
@@ -0,0 +1,8 @@
+// WarApp
+//Create a WarApp class with a main method
+//Create 2 armadas, fill them with ships
+//Have a war!
+package Pirates;
+
+public class WarApp {
+}
