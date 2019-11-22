5f60cdccc2fe739957940959e05654a48fbb6e85
Eszter Viczian <eviczia@gmail.com>
2019-11-22 11:11:25 +0100

    started MaximumFinder

---
+++ week-08_OOP/day-04-recursion/src/MaximumFinder.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+// Maximum finder
+//Write a function that finds the largest element of an array using recursion.
+public class MaximumFinder {
+    public static void main(String[] args) {
+        int[] myArray = {2, 32, 43, 66, 45, 2, 88, 101, 12, 8, 10, 43};
+        int max = findMax(myArray);
+        System.out.println(max);
+
+    }
+
+/*    private static int findMax(int[] myArray) {
+        if (myArray[myArray.length-1] == 0) {
+            return findMax();
+        } else {
+            return Math.max(findMax(Arrays.asList(myArray)), myArray[myArray.length - 1]);
+        }
+    }*/
+}
