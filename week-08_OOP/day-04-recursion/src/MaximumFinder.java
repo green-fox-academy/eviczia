b3ccb4ff247d02d8b2e220bb2483c7c585f3c894
Eszter Viczian <eviczia@gmail.com>
2019-11-25 02:58:50 +0100

    found max

--- week-08_OOP/day-04-recursion/src/MaximumFinder.java
+++ week-08_OOP/day-04-recursion/src/MaximumFinder.java
@@ -4,24 +4,20 @@ import java.util.Arrays;
 //Write a function that finds the largest element of an array using recursion.
 public class MaximumFinder {
     public static void main(String[] args) {
-        int[] myArray = {2000, 32, 43, 6456, 45, 2, 88, 1000, 12, 8, 10, 90};
-        int n = 0;
-        findMax(myArray, n);
-        System.out.println(findMax(myArray, n));
+        int[] myArray = {2000, 32, 43, 6456, 45, 2, 88, 1000, 12, 13, 8, 10, 90};
+        int n = myArray.length;
+        findnMax(myArray, n);
+        System.out.println(findnMax(myArray, n));
 
     }
 
-    private static int findMax(int[] myArray, int n) {
-        int max =0;
-        if (n == 0) {
-            max = myArray[0];
-            System.out.println(max);
-            return max;
+    private static int findnMax(int[] myArray, int n) {
+        if (n == 1) {
+            System.out.println(n);
+            return myArray[0];
         } else {
-            System.out.println(max);
-            max = Math.max(max, findMax(myArray, myArray[n+1]));
-            return max;
-
+            System.out.println(n);
+            return Math.max(myArray[n-1], findnMax(myArray, n-1));
         }
     }
 }
