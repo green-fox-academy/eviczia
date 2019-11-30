d7a524840135aaa17f67a90cf56693e209c91d3b
Eszter Viczian <eviczia@gmail.com>
2019-11-30 06:32:33 +0100

    reformat

--- week-04/day-01/src/DrawDiagonal.java
+++ week-04/day-01/src/DrawDiagonal.java
@@ -1,27 +1,29 @@
+// Write a program that reads a number from the standard input, then draws a
+// square like this:
+//
+//
+// %%%%%
+// %%  %
+// % % %
+// %  %%
+// %%%%%
+//
+// The square should have as many lines as the number was
 public class DrawDiagonal {
-    // Write a program that reads a number from the standard input, then draws a
-    // square like this:
-    //
-    //
-    // %%%%%
-    // %%  %
-    // % % %
-    // %  %%
-    // %%%%%
-    //
-    // The square should have as many lines as the number was
     public static void main(String[] args) {
         int userInput = _PleaseEnterWholeNumber.getUserInput();
         drawHorizontal(userInput);
         drawVerticals(userInput);
         drawHorizontal(userInput);
     }
+
     private static void drawHorizontal(int limit) {
         for (int j = 0; j < limit; j++) {
             System.out.print("%");
         }
         System.out.println();
     }
+
     private static void drawVerticals(int limit) {
         for (int line = 1; line < (limit - 1); line++) {
             System.out.print("%");
