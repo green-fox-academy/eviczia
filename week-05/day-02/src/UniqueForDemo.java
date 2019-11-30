e2e544d78e6fc975a4ae272b0ac8ed012fc7ddf6
Eszter Viczian <eviczia@gmail.com>
2019-11-30 08:29:22 +0100

    reformat

--- week-05/day-02/src/UniqueForDemo.java
+++ week-05/day-02/src/UniqueForDemo.java
@@ -3,19 +3,18 @@ import java.util.Arrays;
 public class UniqueForDemo {
     public static void main(String[] args) {
         //  Create a function that takes a list of numbers as a parameter
-        System.out.println(Arrays.toString(unique(new int[] {1, 11, 34, 11, 52, 61, 1, 34})));
+        System.out.println(Arrays.toString(unique(new int[]{1, 11, 34, 11, 52, 61, 1, 34})));
         //  should print: `[1, 11, 34, 52, 61]`
-
     }
 
-// with the help of Adam Tech https://www.youtube.com/watch?v=xDKW358fHiQ
-    private static int[] unique(int [] listNumbers) {
+    // with the help of Adam Tech https://www.youtube.com/watch?v=xDKW358fHiQ
+    private static int[] unique(int[] listNumbers) {
         int n = listNumbers.length;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; ) {
                 if (listNumbers[i] == listNumbers[j]) {
-                    for (int k = j; k < n-1; k++) {
-                        listNumbers[k] = listNumbers[k+1];
+                    for (int k = j; k < n - 1; k++) {
+                        listNumbers[k] = listNumbers[k + 1];
                     }
                     n--; // decrease n by one after each overwriting
                 } else {
