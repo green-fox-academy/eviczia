516985ae54573ff18f601ce411122b4ec3ce9687
Eszter Viczian <eviczia@gmail.com>
2019-11-30 05:19:58 +0100

    deleted whitespaces

--- week-03/day-03/src/Cuboid.java
+++ week-03/day-03/src/Cuboid.java
@@ -1,16 +1,12 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Cuboid {
-    // Write a program that stores 3 sides of a cuboid as variables (doubles)
+// Write a program that stores 3 sides of a cuboid as variables (doubles)
 // The program should write the surface area and volume of the cuboid like:
-//
-// Surface Area: 600
+//// Surface Area: 600
 // Volume: 1000
-//
-    public static void main(String[] args) {
-        // a, b, c are three sides of a cuboid
 
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println();
@@ -28,7 +24,7 @@ public class Cuboid {
         double c = scanner.nextDouble();
         System.out.println("c = " + c);
 
-        System.out.println("Surface Area: " + (2 * (a*b) + (a*c) + (b*c)));
+        System.out.println("Surface Area: " + (2 * (a * b) + (a * c) + (b * c)));
         System.out.println("Volume: " + (a * b * c));
 
     }
