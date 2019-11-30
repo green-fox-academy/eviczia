33a9cacda96b315f6eb15a4a6908da2734846fb9
Eszter Viczian <eviczia@gmail.com>
2019-11-30 06:49:28 +0100

    added for loop to get user input + reformat

--- week-04/day-01/src/ParametricAverage.java
+++ week-04/day-01/src/ParametricAverage.java
@@ -1,22 +1,18 @@
-public class ParametricAverage {
 // Write a program that asks for a number.
 // It would ask this many times to enter an integer,
 // if all the integers are entered, it should print the sum and average of these
 // integers like:
 //
 // Sum: 22, Average: 4.4
+public class ParametricAverage {
     public static void main(String[] args) {
-        int a = _PleaseEnterWholeNumber.getUserInput();
-        int b = _PleaseEnterWholeNumber.getUserInput();
-        int c = _PleaseEnterWholeNumber.getUserInput();
-        int d = _PleaseEnterWholeNumber.getUserInput();
-        int e = _PleaseEnterWholeNumber.getUserInput();
-
-        int sum = a + b + c + d + e;
-        int ave = (sum /  5);
-// replace 5 by count
+        int sum = 0;
+        int counter = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += _PleaseEnterWholeNumber.getUserInput();
+            counter++;
+        }
+        int ave = (sum / counter);
         System.out.println("Sum: " + sum + ", Average: " + ave);
-
     }
-
 }
