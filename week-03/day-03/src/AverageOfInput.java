05ce726edcc16aa8a6c54c7f2515de423bead8ba
Eszter Viczian <eviczia@gmail.com>
2019-11-30 06:19:41 +0100

    reformat

--- week-03/day-03/src/AverageOfInput.java
+++ week-03/day-03/src/AverageOfInput.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
-
-public class AverageOfInput {
 // Write a program that asks for 5 integers in a row,
 // then it should print the sum and the average of these numbers like:
 //
 // Sum: 22, Average: 4.4
+
+import java.util.Scanner;
+
+public class AverageOfInput {
 
     public static void main(String[] args) {
         System.out.println("Please enter 5 integers!");
@@ -19,8 +20,5 @@ public class AverageOfInput {
 
         System.out.println("Sum: " + (firstNr + secondNr + thirdNr + fourthNr + fifthNr));
         System.out.println("Average: " + (firstNr + secondNr + thirdNr + fourthNr + fifthNr) / 5.);
-
-
     }
-
 }
