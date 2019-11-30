1afcc2849d337414c565b3ac84db29318b11a5e6
Eszter Viczian <eviczia@gmail.com>
2019-11-30 07:02:44 +0100

    reformat

--- week-04/day-01/src/SubStr.java
+++ week-04/day-01/src/SubStr.java
@@ -1,8 +1,16 @@
+//  Create a function that takes two strings as a parameter
+//  Returns the starting index where the second one is starting in the first one
+//  Returns `-1` if the second string is not in the first one
+//  Example:
+//  should print: `17`
+//        System.out.println(subStr("this is what I'm searching in", "searching"));
+//  should print: `-1`
+//        System.out.println(subStr("this is what I'm searching in", "not"));
+
 import java.util.Scanner;
 
 public class SubStr {
     public static void main(String[] args) {
-        //  Create a function that takes two strings as a parameter
         System.out.println("Please enter a string!");
         Scanner scanner = new Scanner(System.in);
         String word1 = scanner.nextLine();
@@ -13,13 +21,6 @@ public class SubStr {
 
         System.out.println(subStr(word1, word2));
     }
-//  Returns the starting index where the second one is starting in the first one
-//  Returns `-1` if the second string is not in the first one
-//  Example:
-//  should print: `17`
-//        System.out.println(subStr("this is what I'm searching in", "searching"));
-//  should print: `-1`
-//        System.out.println(subStr("this is what I'm searching in", "not"));
 
     public static int subStr(String input, String q) {
         int start = input.indexOf(q);
