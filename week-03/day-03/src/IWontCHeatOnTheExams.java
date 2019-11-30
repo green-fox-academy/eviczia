b42d6ca9e31395812fc38b829373a2cdc6196f1c
Eszter Viczian <eviczia@gmail.com>
2019-11-30 06:04:09 +0100

    added indentation

--- week-03/day-03/src/IWontCHeatOnTheExams.java
+++ week-03/day-03/src/IWontCHeatOnTheExams.java
@@ -1,9 +1,9 @@
-public class IWontCHeatOnTheExams {
-    // Create a program that writes this line 100 times:
+// Create a program that writes this line 100 times:
 // "I won't cheat on the exam!"
+public class IWontCHeatOnTheExams {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
-        System.out.println(i + "I won't cheat on the exam!");
+            System.out.println(i + "I won't cheat on the exam!");
         }
     }
 }
