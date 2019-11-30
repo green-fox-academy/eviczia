dfd5f5b55f5bea5b7e813996c51a32c23d5b37a9
Eszter Viczian <eviczia@gmail.com>
2019-11-30 06:22:07 +0100

    reformat

--- week-03/day-03/src/AnimalsAndLegs.java
+++ week-03/day-03/src/AnimalsAndLegs.java
@@ -1,11 +1,12 @@
+// Write a program that asks for two integers
+// The first represents the number of chickens the farmer has
+// The second represents the number of pigs owned by the farmer
+// It should print how many legs all the animals have
+
 import java.util.Scanner;
 
 public class AnimalsAndLegs {
     public static void main(String[] args) {
-        // Write a program that asks for two integers
-        // The first represents the number of chickens the farmer has
-        // The second represents the number of pigs owned by the farmer
-        // It should print how many legs all the animals have
 
         int twoLegged = 2;
         int fourLegged = 4;
@@ -19,8 +20,5 @@ public class AnimalsAndLegs {
         System.out.println();
 
         System.out.println("These animals have " + (chickenNo * twoLegged + pigNo * fourLegged) + " legs altogether");
-
-
-
     }
 }
