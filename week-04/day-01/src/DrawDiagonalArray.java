e5b6df91fad0327c32b961212ad75f26dd9dfa25
Eszter Viczian <eviczia@gmail.com>
2019-11-30 06:34:25 +0100

    reformat

--- week-04/day-01/src/DrawDiagonalArray.java
+++ week-04/day-01/src/DrawDiagonalArray.java
@@ -1,17 +1,4 @@
-import java.util.Arrays;
-
 public class DrawDiagonalArray {
-    // Write a program that reads a number from the standard input, then draws a
-    // square like this:
-    //
-    //
-    // %%%%%
-    // %%  %
-    // % % %
-    // %  %%
-    // %%%%%
-    //
-    // The square should have as many lines as the number was
     public static void main(String[] args) {
         int side = _PleaseEnterWholeNumber.getUserInput();
         int[][] diagonal = new int[side][side];
@@ -24,15 +11,15 @@ public class DrawDiagonalArray {
                 }
             }
         }
-            for (int i = 0; i < diagonal.length; i++) {
-                for (int j = 0; j < diagonal[i].length; j++) {
-                    if (diagonal[i][j]==0) {
-                        System.out.print(" ");
-                    } else {
-                        System.out.print("%");
-                    }
+        for (int i = 0; i < diagonal.length; i++) {
+            for (int j = 0; j < diagonal[i].length; j++) {
+                if (diagonal[i][j] == 0) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("%");
                 }
-                System.out.println();
             }
+            System.out.println();
+        }
     }
 }
