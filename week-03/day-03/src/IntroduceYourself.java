c418773c9fd25e0ebb1f035225726ba2e336fdb8
Eszter Viczian <eviczia@gmail.com>
2019-11-30 05:27:16 +0100

    deleted empty lines and comment

--- week-03/day-03/src/IntroduceYourself.java
+++ week-03/day-03/src/IntroduceYourself.java
@@ -4,21 +4,8 @@ public class IntroduceYourself {
         int myAge = 42;
         float myHeight = 1.68f;
 
-        System.out.println("My name is "+ myName + ".");
+        System.out.println("My name is " + myName + ".");
         System.out.println("I am " + myAge + " years old.");
         System.out.println("I am " + myHeight + " m high.");
-
-
-
-        // Write a program that prints a few details to the terminal window about you
-        // It should print each detail to a new line.
-        //  - Your name
-        //  - Your age
-        //  - Your height in meters (as a decimal fraction)
-        //
-        //  Example output:
-        //  John Doe
-        //  31
-        //  1.87
     }
 }
