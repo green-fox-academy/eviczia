10df0ce0c12d3c8856cd72f500663a1b69543f56
Eszter Viczian <eviczia@gmail.com>
2019-11-30 06:00:22 +0100

    moved comment to top of code, deleted empty lines

--- week-03/day-03/src/MultiplicationTable.java
+++ week-03/day-03/src/MultiplicationTable.java
@@ -1,21 +1,6 @@
 import java.util.Scanner;
 
 public class MultiplicationTable {
-    // Create a program that asks for a number and prints the multiplication table with that number
-//
-// Example:
-// The number 15 should print:
-//
-// 1 * 15 = 15
-// 2 * 15 = 30
-// 3 * 15 = 45
-// 4 * 15 = 60
-// 5 * 15 = 75
-// 6 * 15 = 90
-// 7 * 15 = 105
-// 8 * 15 = 120
-// 9 * 15 = 135
-// 10 * 15 = 150
     public static void main(String[] args) {
         System.out.println("Please enter a whole number!");
         Scanner scanner = new Scanner(System.in);
@@ -24,9 +9,6 @@ public class MultiplicationTable {
         while (i < 10) {
             i++;
             System.out.println(i + " * " + mplyBy + " = " + (i * mplyBy));
-
-
-
         }
     }
 }
