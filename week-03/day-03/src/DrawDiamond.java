7f161f8c4c9aaf4c233d6439dd6006470f74b2b8
Eszter Viczian <eviczia@gmail.com>
2019-11-30 05:44:18 +0100

    added missing space

--- week-03/day-03/src/DrawDiamond.java
+++ week-03/day-03/src/DrawDiamond.java
@@ -15,38 +15,38 @@ public class DrawDiamond {
     // The diamond should have as many lines as the number was
     public static void main(String[] args) {
         int userInput = _PleaseEnterWholeNumber.getUserInput();
-        if ((userInput % 2) ==0) {
+        if ((userInput % 2) == 0) {
             userInput += 1;
         }
 
         int line = 1;
-        while (line <= ((userInput+1)/2)) {
+        while (line <= ((userInput + 1) / 2)) {
             int starNr = 1;
             int spaceNr = 1;
-            while (spaceNr <= ((userInput+1)/2) - line) {
+            while (spaceNr <= ((userInput + 1) / 2) - line) {
                 System.out.print(" ");
-                spaceNr ++;
+                spaceNr++;
             }
             while (starNr <= (2 * line - 1)) {
                 System.out.print("*");
                 starNr++;
             }
             System.out.println();
-            line ++;
+            line++;
         }
         while (line <= userInput) {
             int starNr = 1;
             int spaceNr = 1;
-            while (spaceNr <= (line - (userInput+1)/2)) {
+            while (spaceNr <= (line - (userInput + 1) / 2)) {
                 System.out.print(" ");
-                spaceNr ++;
+                spaceNr++;
             }
-            while (starNr <= (2 * (userInput - line)+1)) {
+            while (starNr <= (2 * (userInput - line) + 1)) {
                 System.out.print("*");
                 starNr++;
             }
             System.out.println();
-            line ++;
+            line++;
         }
     }
 }
