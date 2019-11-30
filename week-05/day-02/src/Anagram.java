b58fd1f17ed7a473f16da1bcb1f99e8f23992929
Eszter Viczian <eviczia@gmail.com>
2019-11-30 08:43:48 +0100

    wrote a function to check for palindromes

--- week-05/day-02/src/Anagram.java
+++ week-05/day-02/src/Anagram.java
@@ -7,9 +7,24 @@ public class Anagram {
     //"dog"	"god"	true
     //"green"	"fox"	false
     public static void main(String[] args) {
-//        System.out.println(isAnagram("dog", "god"));
-//        System.out.println(isAnagram("green", "fox"));
+        System.out.println(isAnagram("dog", "god"));
+        System.out.println(isAnagram("green", "fox"));
 
+    }
 
+    private static boolean isAnagram(String word1, String word2) {
+        if (word1.length() != word2.length()) {
+            return false;
+        } else {
+            int sumTrues = 0;
+            for (int i = 0; i < word1.length(); i++) {
+                if (word1.charAt(i) == word2.charAt(word2.length()-i-1)) {
+                    sumTrues++;
+                }
+            } if (sumTrues == word1.length()) {
+                return true;
+            }
+        }
+        return false;
     }
 }
