52c9e7327820e09d997d4b345533232a06baffc0
Eszter Viczian <eviczia@gmail.com>
2019-11-30 06:23:17 +0100

    reformat

--- week-03/day-03/src/MileToKmConverter.java
+++ week-03/day-03/src/MileToKmConverter.java
@@ -1,16 +1,17 @@
+// Write a program that asks for a double that is a distance in miles,
+// then it converts that value to kilometers and prints it
+
 import java.util.Scanner;
 
 public class MileToKmConverter {
     public static void main(String[] args) {
-        // Write a program that asks for a double that is a distance in miles,
-        // then it converts that value to kilometers and prints it
 
         System.out.println("How many miles did you walk yesterday?");
         Scanner scanner = new Scanner(System.in);
         Double walkedMiles = scanner.nextDouble();
         // 1 mile = 1.609344 kilometers
 
-        System.out.println("You walked " + (walkedMiles*1.61) + " km yesterday!");
+        System.out.println("You walked " + (walkedMiles * 1.61) + " km yesterday!");
 
     }
 }
