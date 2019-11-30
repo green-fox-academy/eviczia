376606ae1b7c9d9fb8d3bdc921bcab23131c0623
Eszter Viczian <eviczia@gmail.com>
2019-11-30 07:28:08 +0100

    reformat

--- week-05/day-01/src/DiagonalMatrixSingleForLoop.java
+++ week-05/day-01/src/DiagonalMatrixSingleForLoop.java
@@ -1,39 +1,39 @@
+// - Create (dynamically) a two dimensional array
+//   with the following matrix. Use a loop!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Print this two dimensional array to the output
+
 import java.util.Arrays;
 
 public class DiagonalMatrixSingleForLoop {
-    // - Create (dynamically) a two dimensional array
-    //   with the following matrix. Use a loop!
-    //
-    //   1 0 0 0
-    //   0 1 0 0
-    //   0 0 1 0
-    //   0 0 0 1
-    //
-    // - Print this two dimensional array to the output
     public static void main(String[] args) {
         int side = 4;
         int content = 0;
         int d = 1;
         int[][] diagonal = diagonalMatrix(side, content, d);
         printMatrix(diagonal);
-
     }
-    public static int[][] diagonalMatrix (int side, int content, int d) {
+
+    public static int[][] diagonalMatrix(int side, int content, int d) {
         int[][] matrix = new int[side][side];
         for (int i = 0; i < matrix.length; i++) {
             Arrays.fill(matrix[i], content);
             matrix[i][i] = d;
         }
-        return  matrix;
+        return matrix;
     }
 
-    public static void printMatrix (int[][] matrix) {
+    public static void printMatrix(int[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
-
         }
     }
 
