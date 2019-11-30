67cf235dcdaba451c50cc2a1e939b04aafda7c32
Eszter Viczian <eviczia@gmail.com>
2019-11-30 07:47:41 +0100

    reformat

--- week-05/day-02/src/Sum.java
+++ week-05/day-02/src/Sum.java
@@ -1,18 +1,18 @@
+// Write a function called `sum` that returns the sum of numbers from zero to the given parameter
+
 import java.util.Scanner;
 
 public class Sum {
-// Create the usual class wrapper and main method on your own.
-// Write a function called `sum` that returns the sum of numbers from zero to the given parameter
     public static void main(String[] args) {
         System.out.println("Please enter a number!");
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
         int sumUpTo = scanner.nextInt();
         System.out.println(sum(sumUpTo));
     }
 
     private static int sum(int input) {
         int output = 0;
-        for (int i = 0; i <= input ; i++) {
+        for (int i = 0; i <= input; i++) {
             output += i;
         }
         return output;
