803d747dab060a7944357cefdbee78621aef3cde
Eszter Viczian <eviczia@gmail.com>
2019-11-30 19:31:59 +0100

    reformat

--- week-06/day-4-drawing_project/src/LinePlay_quarters.java
+++ week-06/day-4-drawing_project/src/LinePlay_quarters.java
@@ -4,25 +4,28 @@ import java.awt.*;
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class LinePlay_quarters {
+    // Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 320;
+
     public static void mainDraw(Graphics graphics) {
         // draw a red horizontal line to the canvas' middle.
         // draw a green vertical line to the canvas' middle.
         int density = 20;
         int splitInto = 4;
-        int w = WIDTH/splitInto*2;
-        int h = HEIGHT/splitInto*2;
+        int w = WIDTH / splitInto * 2;
+        int h = HEIGHT / splitInto * 2;
         int startX = 0;
         int startY = 0;
         drawImage(startX, startY, density, w, h, splitInto, graphics);
-
     }
 
     private static void drawImage(int startX, int startY, int density, int w, int h, int splitInto, Graphics graphics) {
         for (int i = 0; i < 2; i++) {
-                startX = startX + i*w;
-                startY = startY + i*h;
-                lineplayNE(startX, startY, density, w, h, graphics);
-                lineplaySW(startX, startY, density, w, h, graphics);
+            startX = startX + i * w;
+            startY = startY + i * h;
+            lineplayNE(startX, startY, density, w, h, graphics);
+            lineplaySW(startX, startY, density, w, h, graphics);
         }
     }
 
@@ -42,17 +45,11 @@ public class LinePlay_quarters {
             for (int j = startY + 1; j <= h; j = j + h / density) {
                 if (Math.abs(i - j) == 1) {
                     graphics.setColor(new Color(200, 32, 255));
-                    graphics.drawLine(startX+i, startY+ 0, startX+h, startY+j);
+                    graphics.drawLine(startX + i, startY + 0, startX + h, startY + j);
                 }
             }
         }
     }
-
-
-
-    // Don't touch the code below
-    static int WIDTH = 320;
-    static int HEIGHT = 320;
 
     public static void main(String[] args) {
         JFrame jFrame = new JFrame("Drawing");
