caa790bacfe6274070531fd0ffee148817d36988
Eszter Viczian <eviczia@gmail.com>
2019-11-30 07:39:34 +0100

    reformat

--- week-05/day-01/src/Reverse.java
+++ week-05/day-01/src/Reverse.java
@@ -1,19 +1,20 @@
-import java.util.Arrays;
-
-public class Reverse {
 // - Create an array variable named `aj`
 //   with the following content: `[3, 4, 5, 6, 7]`
 // - Reverse the order of the elements in `aj`
 // - Print the elements of the reversed `aj`
+
+import java.util.Arrays;
+
+public class Reverse {
     public static void main(String[] args) {
         int[] aj = {3, 4, 5, 6, 7};
         int index = 0;
         int temp = 0;
 
-        while (index < (aj.length/2)) {
+        while (index < (aj.length / 2)) {
             temp = aj[index];
-            aj[index] = aj[aj.length-index-1];
-            aj[aj.length-index-1] = temp;
+            aj[index] = aj[aj.length - index - 1];
+            aj[aj.length - index - 1] = temp;
             index++;
         }
         System.out.println(Arrays.toString(aj));
