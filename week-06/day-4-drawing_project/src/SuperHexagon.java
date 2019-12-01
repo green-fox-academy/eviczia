e9d4d20941d6caad2f201952def1dabc8c3910f2
Eszter Viczian <eviczia@gmail.com>
2019-12-01 02:42:46 +0100

    refactor

--- week-06/day-4-drawing_project/src/SuperHexagon.java
+++ week-06/day-4-drawing_project/src/SuperHexagon.java
@@ -12,40 +12,46 @@ public class SuperHexagon {
 
 // define parameters
         int side = 30; // side length of one hexagon
-        int halfSide = side / 2;
         int halfHeight = (int) (Math.sqrt(3) * side) / 2; // half height of one hexagon (height of "side" based eq.triangle)
         int w = WIDTH / 2;
         int h = HEIGHT / 2;
         int layers = 3; // number of concentric circles of hexagons (including middle)
         // if we want to change the layers, we need to manually set the x,y array length and then
         // the if condition to omit hexagons
-        int[] x = new int[layers + 2]; // define array to hold x coordinates of hexagon centers
-        for (int i = 0; i < x.length; i++) {
-            x[i] = (int) (w + (i - layers + 1) * 1.5 * side);
+
+        int[] xHexCenters = findXs(w,side,layers);
+        int[] yHexCenters = findYs(h, halfHeight, layers);
+// pair the xy coordinates to form the center of the hexagons,
+// from starting from hexagon center, define  each hexagons' angles' coordinates as parameters.
+        for (int i = 0; i < yHexCenters.length; i++) {
+            for (int j = 0; j < xHexCenters.length; j++) {
+                // we do not want to draw hexagon to every X-Y coordinate pair
+//docs.google.com/spreadsheets/d/1jblIqXlIv7JqDDyNh1l_XEoDsoPMjIdWxOJtttl0ltA/edit#gid=852833476
+                if (((i + j) % 2 == 0) && ((i + j != 0) && (i + j != 12) && (i - j != 8) && (j - i != 4))) {
+                    int[] xToDrawAround = new int[]{xHexCenters[j] - side / 2, xHexCenters[j] + side / 2, xHexCenters[j] + side, xHexCenters[j] + side / 2, xHexCenters[j] - side / 2, xHexCenters[j] - side,};
+                    int[] yToDrawAround = new int[]{yHexCenters[i] - halfHeight, yHexCenters[i] - halfHeight, yHexCenters[i], yHexCenters[i] + halfHeight, yHexCenters[i] + halfHeight, yHexCenters[i]};
+                    int n = 6;
+                    graphics.setColor(Color.BLUE);
+                    graphics.drawPolygon(xToDrawAround, yToDrawAround, n);
+                }
+            }
         }
+    }
+
+    private static int[] findYs(int h, int halfHeight, int layers) {
         int[] y = new int[layers + 6]; // define array to hold y coordinates of hexagon centers
         for (int i = 0; i < y.length; i++) {
             y[i] = h + (i - layers - 1) * halfHeight;
         }
-// pair the xy coordinates to form the center of the hexagons,
-// from starting from hexagon center, define  each hexagons' angles' coordinates as parameters.
-        for (int i = 0; i < y.length; i++) {
-            for (int j = 0; j < x.length; j++) {
-                // we do not want to draw hexagon to every X-Y coordinate pair
-                see:
-                https:
-//docs.google.com/spreadsheets/d/1jblIqXlIv7JqDDyNh1l_XEoDsoPMjIdWxOJtttl0ltA/edit#gid=852833476
-                if (((i + j) % 2 == 0) && ((i + j != 0) && (i + j != 12) && (i - j != 8) && (j - i != 4))) {
-                    int[] xx = new int[]{x[j] - side / 2, x[j] + side / 2, x[j] + side, x[j] + side / 2, x[j] - side / 2, x[j] - side,};
-                    int[] yy = new int[]{y[i] - halfHeight, y[i] - halfHeight, y[i], y[i] + halfHeight, y[i] + halfHeight, y[i]};
+        return y;
+    }
 
-                    //     Polygon hex = new Polygon(xx, yy,side);
-                    int n = 6;
-                    graphics.setColor(Color.BLUE);
-                    graphics.drawPolygon(xx, yy, n);
-                }
-            }
+    private static int[] findXs(int w, int side, int layers) {
+        int[] x = new int[layers + 2]; // define array to hold x coordinates of hexagon centers
+        for (int i = 0; i < x.length; i++) {
+            x[i] = (int) (w + (i - layers + 1) * 1.5 * side);
         }
+        return x;
     }
 
     public static void main(String[] args) {
