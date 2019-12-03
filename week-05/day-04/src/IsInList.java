dc828053e6d77694d95ca5843cd07197e26ef4d9
Eszter Viczian <eviczia@gmail.com>
2019-12-03 05:25:36 +0100

    simplified return boolean value

--- week-05/day-04/src/IsInList.java
+++ week-05/day-04/src/IsInList.java
@@ -1,28 +1,26 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
-public class IsInList{
-    public static void main(String... args){
+public class IsInList {
+    public static void main(String... args) {
         ArrayList<Integer> list = new ArrayList<>(Arrays.asList(2, 4, 6, 8, 10, 12, 14, 16));
-        checkNums(list);
+        System.out.println(checkNums(list));
         // Check if list contains all of the following elements: 4,8,12,16
         // Create a method that accepts list as an input
         // it should return "true" if it contains all, otherwise "false"
 
     }
 
-    private static void checkNums(ArrayList<Integer> list) {
+    private static boolean checkNums(ArrayList<Integer> list) {
         ArrayList<Integer> checklist = new ArrayList<>(Arrays.asList(4, 8, 12, 16));
-        Boolean has4_8_12_16 = false;
         int counter = 0;
         for (int i = 0; i < list.size(); i++) {
             for (int j = 0; j < checklist.size(); j++) {
-                if (list.get(i)== checklist.get(j)) {
-                    counter +=1;
+                if (list.get(i) == checklist.get(j)) {
+                    counter += 1;
                 }
             }
-        }if (counter == checklist.size()) {
-            has4_8_12_16 = true;
         }
-        System.out.println(has4_8_12_16);
+        return counter == checklist.size();
     }
 }
