1b308ca1e79843e15353cadb4d3b5becdd611deb
Eszter Viczian <eviczia@gmail.com>
2019-12-03 04:33:22 +0100

    solved urlFixer with split

--- week-05/day-03/src/UrlFixer.java
+++ week-05/day-03/src/UrlFixer.java
@@ -5,10 +5,15 @@ public class UrlFixer {
         // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
         // Also, the URL is missing a crutial component, find out what it is and insert it too!
         // Try to solve it more than once using different String functions!
-        String urlFixed = url.replace("https", "https:").replace("bots", "odds");
-
-        url = urlFixed;
+        url = url.replace("https", "https:").replace("bots", "odds");
 
         System.out.println(url);
+
+        url = url.split("https")[0] + "https:" + url.split("https")[1];
+        System.out.println(url.split("https").length);
+        url = url.split("bots")[0] + "odds";
+        System.out.println(url.split("bots").length);
+
+
     }
 }
