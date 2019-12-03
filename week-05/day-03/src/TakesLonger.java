ab422b071f5340c70c7a09a84db14e0dc32369ef
Eszter Viczian <eviczia@gmail.com>
2019-12-03 04:40:29 +0100

    solved TakesLonger with split and StringBuilder

--- week-05/day-03/src/TakesLonger.java
+++ week-05/day-03/src/TakesLonger.java
@@ -1,12 +1,17 @@
 public class TakesLonger {
-    public static void main(String... args){
+    public static void main(String... args) {
         String quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.";
 
         // When saving this quote a disk error has occured. Please fix it.
         // Add "always takes longer than" to the StringBuilder (quote) between the words "It" and "you"
         // Using pieces of the quote variable (instead of just redefining the string)
 
-        quote = quote.replace("It you", "It always takes longer than you" );
+        StringBuilder quoteRight = new StringBuilder();
+        quoteRight.append(quote.split("It you")[0]);
+        quoteRight.append("It always takes longer than you");
+        quoteRight.append(quote.split("It you")[1]);
+
+        quote = quoteRight.toString();
 
         System.out.println(quote);
     }
