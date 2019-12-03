4bf26c4487d27ef6c551d96532a7f5d8290b6cff
Eszter Viczian <eviczia@gmail.com>
2019-12-03 02:36:41 +0100

    createdd function to sort array ascending

---
+++ week-05/day-02/src/Bubble.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+public class Bubble {
+    public static void main(String[] args) {
+        //  Create a function that takes a list of numbers as parameter
+        //  Returns a list where the elements are sorted in ascending numerical order
+        //  Make a second boolean parameter, if it's `true` sort that list descending
+
+        //  Example:
+        System.out.println(bubble(new int[]{34, 12, 24, 9, 5}));
+        //  should print [5, 9, 12, 24, 34]
+//        System.out.println(advancedBubble(new int[] {34, 12, 24, 9, 5}, true));
+        //  should print [34, 24, 12, 9, 5]
+    }
+
+    private static String bubble(int[] ints) {
+        System.out.println(Arrays.toString(ints));
+        int n = ints.length;
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 1; j <= Math.min(n - i, i); j++) {
+                int temp = ints[i];
+                if (ints[i] < ints[i - j]) {
+                    ints[i] = ints[i - j];
+                    ints[i - j] = temp;
+                    System.out.println("done" + i + j);
+                    System.out.println(Arrays.toString(ints));
+                }
+            }
+        }
+        for (int i = n - 1; i > 0; i--) {
+            if (ints[i] < ints[i - 1]) {
+                bubble(ints);
+            }
+        }
+        return Arrays.toString(ints);
+    }
+}
