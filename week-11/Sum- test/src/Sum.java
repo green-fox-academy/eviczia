83fc547fb0d676c3b7017c44056c22d609d09fa8
Eszter Viczian <eviczia@gmail.com>
2019-12-11 11:52:24 +0100

    created sumArray method, added first testCase

---
+++ week-11/Sum- test/src/Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+
+
+    public int sum(int[] listOfIntegers) {
+        int sum = 0;
+        for (int number : listOfIntegers) {
+            sum += number;
+        } return sum;
+    }
+}
