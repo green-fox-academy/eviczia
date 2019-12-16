f634fb73452a17cc86ff6edd5c374590c719d4c4
Eszter Viczian <eviczia@gmail.com>
2019-12-16 03:16:51 +0100

    added test directory yet to elaborate test cases

---
+++ week-11/CowsAndBulls/Test/CowsAndBullsTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CowsAndBullsTest {
+
+    @Test
+    public void playCowsAndBulls() {
+    }
+
+    @Test
+    public void getUserInput_Accepts4digitNumber() {
+
+    }
+
+    @Test
+    public void numberToHashMap() {
+    }
+
+    @Test
+    public void compareNumbers() {
+    }
+
+    @Test
+    public void cowBullResult() {
+    }
+}
--- week-11/CowsAndBulls/src/CowsAndBulls.java
+++ week-11/CowsAndBulls/src/CowsAndBulls.java
@@ -1,6 +1,3 @@
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -12,29 +9,9 @@ public class CowsAndBulls {
     private int countGuesses;
 
     public CowsAndBulls() {
-        numberToGuess = (int)(Math.random()*9000+1000);
+        numberToGuess = (int) (Math.random() * 9000 + 1000);
         isPlaying = true;
         countGuesses = 0;
-    }
-
-    public int getNumberToGuess() {
-        return numberToGuess;
-    }
-
-    public String isPlaying() {
-        return (isPlaying ? "playing" : "finished");
-    }
-
-    public void setPlaying(boolean playing) {
-        isPlaying = playing;
-    }
-
-    public int getCountGuesses() {
-        return countGuesses;
-    }
-
-    public void setCountGuesses(int countGuesses) {
-        this.countGuesses = countGuesses;
     }
 
     public void playCowsAndBulls() {
@@ -42,22 +19,22 @@ public class CowsAndBulls {
 
         int[] result = new int[2];
 
-        while (result[0] < 4 ) {
+        while (result[0] < 4) {
             countGuesses++;
             int guess = getUserInput();
             result = compareNumbers(guess);
             System.out.println(cowBullResult(result));
-            if (result[0] == 4) break;
         }
         System.out.printf("You win! You got it from %d guesses.\n", countGuesses);
     }
 
-    public int getUserInput () {
+    public int getUserInput() {
         Scanner scanner = new Scanner(System.in);
         int guess = scanner.nextInt();
         if (guess < 1000 || guess > 9999) {
             getUserInput();
-        } return guess;
+        }
+        return guess;
     }
 
     public HashMap<String, Integer> numberToHashMap(int fourDigitNumber) {
@@ -73,7 +50,8 @@ public class CowsAndBulls {
 
         return fourDigitsHashMap;
     }
-    public int[] compareNumbers (int guess) {
+
+    public int[] compareNumbers(int guess) {
         int cowCounter = 0;
         int bullCounter = 0;
         HashMap<String, Integer> myGuess = this.numberToHashMap(guess);
@@ -84,7 +62,7 @@ public class CowsAndBulls {
                 if (digitGuess.getKey() == digitNumber.getKey() & digitGuess.getValue() == digitNumber.getValue()) {
                     cowCounter++;
                     guessThis.remove(digitNumber.getKey());
-                    myGuess.replace((String)digitGuess.getKey(), -1);
+                    myGuess.replace((String) digitGuess.getKey(), -1);
                     break;
                 }
             }
@@ -94,7 +72,7 @@ public class CowsAndBulls {
                 if (digitGuess.getValue() == digitNumber.getValue()) {
                     bullCounter++;
                     guessThis.remove(digitNumber.getKey());
-                    myGuess.replace((String)digitGuess.getKey(), -1);
+                    myGuess.replace((String) digitGuess.getKey(), -1);
                     break;
                 }
             }
