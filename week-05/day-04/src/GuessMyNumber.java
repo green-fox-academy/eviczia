3a60b3bfda01b9d38230d51c34f5dbd35b64a233
Eszter Viczian <eviczia@gmail.com>
2019-12-28 21:00:04 +0100

    finished guessmynumber

--- week-05/day-04/src/GuessMyNumber.java
+++ week-05/day-04/src/GuessMyNumber.java
@@ -5,14 +5,11 @@ public class GuessMyNumber {
     private static int secretNumber;
 
     public static void main(String[] args) {
-
         getParameters();
-
-        playgame();
+        playGame();
     }
 
-
-    private static void playgame() {
+    private static void playGame() {
         System.out.println("Guess!");
         while (guesses > 0) {
             Scanner scanner = new Scanner(System.in);
@@ -26,18 +23,21 @@ public class GuessMyNumber {
             System.out.printf("Too %s. You have %d lives left.\n", result, guesses);
         }
         System.out.println("The number was: " + secretNumber);
-
-
     }
 
+
     private static void getParameters() {
+
         System.out.println("I will think of a number. Please give me a minimum limit.");
         Scanner scanner = new Scanner(System.in);
         int min = scanner.nextInt();
+
         System.out.printf("My number will be higher than %d. Now please give me a maximum limit.\n", min);
         int max = scanner.nextInt();
+
         System.out.printf("My number will be between %d and %d. How many guesses do you need?\n", min, max);
         guesses = scanner.nextInt();
+
         System.out.printf("I've thought of a number between %d-%d. You have %d lives.\n", min, max, guesses);
 
         secretNumber = (int) (Math.random() * (max - min + 1) + min);
