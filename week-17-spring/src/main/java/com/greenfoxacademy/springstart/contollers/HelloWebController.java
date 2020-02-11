04248519971d0995be07553b848c896792bc5481
Eszter Viczian <eviczia@gmail.com>
2020-02-11 11:13:01 +0100

    greet and count

--- week-17-spring/src/main/java/com/greenfoxacademy/springstart/contollers/HelloWebController.java
+++ week-17-spring/src/main/java/com/greenfoxacademy/springstart/contollers/HelloWebController.java
@@ -3,13 +3,22 @@ package com.greenfoxacademy.springstart.contollers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 public class HelloWebController {
+    private AtomicLong numberOfCalls = new AtomicLong(0);
+
 
     @RequestMapping("/web/greeting")
-    public String greeting(Model model) {
-        model.addAttribute("name", " World");
+    public String greeting(@RequestParam(value = "name", required = false) String name, Model model) {
+
+        numberOfCalls.incrementAndGet();
+
+        model.addAttribute("name", name);
+        model.addAttribute("loads", numberOfCalls);
         return "greeting";
     }
 }
