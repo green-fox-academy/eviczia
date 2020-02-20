18839efeec0fad0dc6f56d2a26f8a18adb521ca0
Eszter Viczian <eviczia@gmail.com>
2020-02-20 10:16:08 +0100

    hellobeanworld - dependency injection

---
+++ week-18-dependency-injection/src/main/java/com/greenfox/hellobeanworld/Printer.java
@@ -0,0 +1,18 @@
+package com.greenfox.hellobeanworld;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class Printer {
+
+    public Printer() {
+    }
+
+    public void log(String message) {
+        System.out.println(LocalDateTime.now() + " MY PRINTER SAYS --- " + message);
+    }
+}
+
