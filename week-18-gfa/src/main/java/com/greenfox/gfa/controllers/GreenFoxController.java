7e84ce47ae7d0340a0bc76ff93ba9da918d56597
Eszter Viczian <eviczia@gmail.com>
2020-02-22 18:37:57 +0100

    began gfa exercise

---
+++ week-18-gfa/src/main/java/com/greenfox/gfa/controllers/GreenFoxController.java
@@ -0,0 +1,30 @@
+package com.greenfox.gfa.controllers;
+
+import com.greenfox.gfa.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GreenFoxController {
+
+    private StudentService studentService;
+
+    @Autowired
+    public GreenFoxController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping("/gfa")
+    public String gfaHome() {
+        return "index";
+    }
+
+    @GetMapping("/gfa/list")
+    public String listNames(Model model) {
+        model.addAttribute("names", studentService.findAll());
+
+        return "list";
+    }
+}
